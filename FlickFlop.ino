3a3b7625df519b601d6633d04aa5d327481dd360
Jonathan Bobrow <jbobrow@gmail.com>
2018-10-18 19:11:43 -0400

    working prototype - Alpha

--- FlickFlop.ino
+++ FlickFlop.ino
@@ -8,7 +8,7 @@
 */
 
 #define FLOPPER_INTERVAL 2000         // time between team flops
-#define FLOPPER_INTERVAL_RANGE 0      // interval can be +/- this amount
+#define FLOPPER_INTERVAL_RANGE 1000   // interval can be +/- this amount
 //#define NUM_TEAMS 3                 // let's make this variable to playtest
 
 Timer flopperTimer;
@@ -20,8 +20,9 @@ enum gameRole {
 };
 
 byte role = FLICKER;
+byte teamBroadcast = 0;
 byte team = 0;
-byte numTeams = 4;
+byte numTeams = 3;
 bool isCommittedToTeam = false;
 bool wasAlone = false;
 
@@ -45,16 +46,18 @@ void loop() {
     }
   }
 
-  if (isAlone() && buttonPressed() ) {
-    if (isCommittedToTeam) {
-      // reset our team
-      isCommittedToTeam = false;
-    }
-    else {
-      // change team
-      team++;
-      if (team > numTeams) {
-        team == 1;
+  if (buttonPressed()) {
+    if (role == FLICKER && isAlone()) {
+      if (isCommittedToTeam) {
+        // reset our team
+        isCommittedToTeam = false;
+      }
+      else {
+        // change team
+        team++;
+        if (team > numTeams) {
+          team = 1;
+        }
       }
     }
   }
@@ -80,8 +83,8 @@ void updateFlopper() {
   // I'm a flopper, all I have to do is change which team I am broadcasting every so often...
   if (flopperTimer.isExpired()) {
     // let's flop!
-    team = getRandomTeam(team);
-    flopperTimer.set(FLOPPER_INTERVAL + rand(FLOPPER_INTERVAL_RANGE) - FLOPPER_INTERVAL_RANGE/2); 
+    team = team % numTeams + 1;  //getRandomTeam(team);
+    flopperTimer.set(FLOPPER_INTERVAL + rand(FLOPPER_INTERVAL_RANGE * 2) - FLOPPER_INTERVAL_RANGE);
   }
 
   // broadcast our team color
@@ -91,7 +94,7 @@ void updateFlopper() {
 byte getRandomTeam( byte curTeam) {
   byte t = 255;
   while ( t != curTeam ) {
-    t = 1 + rand(numTeams);
+    t = 1 + rand(numTeams - 1);
   }
   return t;
 }
@@ -105,12 +108,14 @@ void updateFlicker() {
       // if I was alone and now have a neighbor, take the team broadcast from the flopper
       if (wasAlone) {
         team = neighbor;
+        isCommittedToTeam = true; // We're now this team for real
         wasAlone = false;
         break;
       }
       else {
         // if I hear the flopper update its team, broadcast that team
         if (neighbor != 0) {
+          teamBroadcast = neighbor;
           setValueSentOnAllFaces(neighbor);
           break;
         }
@@ -118,7 +123,7 @@ void updateFlicker() {
     }
   }
 
-  if (isAlone()) {
+  if (isAlone() && !isCommittedToTeam ) {
     wasAlone = true;
   }
 
@@ -139,11 +144,18 @@ void displayFlicker() {
   if (isCommittedToTeam) {
     // solid for our team color
     setColor( getColorForTeam(team) );
+    if (!isAlone()) {
+      byte index = (millis() / 300) % 6;
+      setColorOnFace( getColorForTeam(teamBroadcast), index  );
+      setColorOnFace( getColorForTeam(teamBroadcast), (index + 1) % 6);
+      setColorOnFace( getColorForTeam(teamBroadcast), (index + 2) % 6 );
+    }
   }
   else {
     // one light to root for our team
     setColorOnFace( getColorForTeam(team), 0 );
   }
+
 }
 
 Color getColorForTeam( byte t ) {
@@ -153,8 +165,8 @@ Color getColorForTeam( byte t ) {
   switch (t) {
     case 1: c = makeColorHSB(160, 255, 255); break;
     case 2: c = makeColorHSB(200, 255, 255); break;
-    case 3: c = makeColorHSB(240, 255, 255); break;
-    case 4: c = makeColorHSB(280, 255, 255); break;
+    case 3: c = makeColorHSB(280, 255, 255); break;
+    case 4: c = makeColorHSB(30, 255, 255); break;
     default: WHITE;
   }
 
