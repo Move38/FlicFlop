9e9accc6653ebbd498315feda5334ed6151a6ed8
Daniel King <reluctantpirategames@gmail.com>
2019-07-26 17:41:57 -0400

    Began new game flow, mostly cuts

--- FlicFlop.ino
+++ FlicFlop.ino
@@ -1,263 +1,95 @@
-/*
-   Flick Flop
-
-    1 piece becomes a flippin' master
-    other pieces are game pieces. once attached they take the masters color
-
-   by Nick Bentley, Jonathan Bobrow, Dan King
-*/
-
 enum blinkTypes {FLOPPER, FLICKER};
 byte blinkType = FLICKER;
 
-#define TEAM_COUNT 3
 byte scoringTeam = 0;
 byte signalTeam = 0;
-
-byte displayColor = 0;
-
-enum gameStates {GAME, TRANSITION, SCORE};
-byte gameState = GAME;
+byte teamHues[4] = {0, 45, 125, 230};
 
 Timer flopTimer;
 #define FLOP_INTERVAL 2000
 
-
 Timer animTimer;
 #define ANIMATION_INTERVAL 200
 byte spinFace = 0;
-byte teamHues[4] = {0, 45, 125, 230};
 
-void setup() {
-  // put your setup code here, to run once:
-
-}
+enum celebrationStates {INERT, CELEBRATE, RESOLVING};
+byte celebrationState = INERT;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (blinkType == FLOPPER) {
-    flopperLoop();
-    flopperDisplay();
-  } else if (blinkType == FLICKER) {
+
+  //RUN LOOPS
+  if (blinkType == FLICKER) {
     flickerLoop();
     flickerDisplay();
+  } else if (blinkType == FLOPPER) {
+    flopperLoop();
+    flopperDisplay();
   }
 
-  //set up communication
-  byte sendData = (blinkType << 5) + (gameState << 3) + (signalTeam);
-  setValueSentOnAllFaces(sendData);
+  //SET UP COMMUNICATION
+  byte sendData = (signalTeam << 5) + (scoringTeam << 3) + (celebrationState);
 
-  //dump button presses
-  buttonSingleClicked();
+  //DUMP BUTTON PRESSES
   buttonDoubleClicked();
-}
+  buttonLongPressed();
 
-void flopperLoop() {
-  if (gameState == GAME) {
-    //time to change team?
-    if (flopTimer.isExpired()) {
-      signalTeam = (signalTeam % TEAM_COUNT) + 1;
-      flopTimer.set(FLOP_INTERVAL);
-    }
-
-    //if double clicked, move to transition
-    if (buttonDoubleClicked()) {
-      gameState = TRANSITION;
-    }
-  } else if (gameState == TRANSITION) {
-    if (scoreCheck()) {
-      gameState = SCORE;
-      signalTeam = 1;
-    }
-  } else if (gameState == SCORE) {
-    if (buttonDoubleClicked()) {
-      if (isAlone()) {
-        gameState = GAME;
-      }
-    }
-  }
-
-  //change to flopper?
-  if (buttonLongPressed()) {
-    if (isAlone) {
-      blinkType = FLICKER;
-      scoringTeam = 0;
-      signalTeam = 0;
-      gameState = GAME;
-    }
-  }
 }
 
 void flickerLoop() {
-  if (gameState == GAME) {
-    if (signalTeam > 0) { //I'm in the game itself
-      bool stillChecking = true;
-      FOREACH_FACE(f) {
-        if (stillChecking) {
-          if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
 
-            //team change?
-            if (getSignalTeam(getLastValueReceivedOnFace(f)) == 1 && signalTeam == 3) {
-              signalTeam = 1;
-              stillChecking = false;
-            } else if (getSignalTeam(getLastValueReceivedOnFace(f)) == 2 && signalTeam == 1) {
-              signalTeam = 2;
-              stillChecking = false;
-            } else if (getSignalTeam(getLastValueReceivedOnFace(f)) == 3 && signalTeam == 2) {
-              signalTeam = 3;
-              stillChecking = false;
-            }
-          }
-        }
-      }
+  if (signalTeam == 0) {
 
-    } else {//Im just a flicker in waiting
-      //wait for a neighbor to appear who is part of the game
-      FOREACH_FACE(f) {
-        if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
-          if (getSignalTeam(getLastValueReceivedOnFace(f)) > 0) {
-            scoringTeam = getSignalTeam(getLastValueReceivedOnFace(f));
-            signalTeam = scoringTeam;
-          }
-        }
-      }
+    //look for neighbors with a signal
+    FOREACH_FACE(f) {
+      
     }
 
-    //also, time to transition?
-    if (transitionCheck()) {
-      gameState = TRANSITION;
-      signalTeam = 0;
+  } else {
+
+    //listen for going alone
+    if (isAlone()) {
+
     }
 
-  } else if (gameState == TRANSITION) {
-    if (scoreCheck()) {
-      gameState = SCORE;
-    }
-  } else if (gameState == SCORE) {
-    if (buttonDoubleClicked() || gameCheck()) {
-      gameState = GAME;
-      scoringTeam = 0;
-    }
   }
 
-  //change to flicker?
+  if (scoringTeam == 0) {
+
+    //listen for long press to become flopper
+
+  } else {
+
+    //listen for signal change
+
+  }
+
   if (buttonLongPressed()) {
-    if (isAlone) {
-      blinkType = FLOPPER;
-      scoringTeam = 0;
-      signalTeam = 0;
-      gameState = GAME;
-    }
+    signalTeam = 1;
+    scoringTeam = 0;
+    blinkType = FLOPPER;
   }
 }
 
-/////////////////
-//DISPLAY LOOPS//
-/////////////////
+void flopperLoop() {
 
-void flopperDisplay() {
-  if (animTimer.isExpired()) {
-    spinFace = (spinFace + 1) % 6;
-    animTimer.set(ANIMATION_INTERVAL);
+  if (flopTimer.isExpired()) {
+    signalTeam += 1;
+    if (signalTeam == 4) {
+      signalTeam = 1;
+    }
   }
 
-  if (gameState == GAME) {
-    setColor(makeColorHSB(teamHues[signalTeam], 255, 255));
-    setColorOnFace(OFF, spinFace);
-  } else {
-    FOREACH_FACE(f) {
-      setColorOnFace(makeColorHSB(teamHues[(f / 2) + 1], 255, 255), (spinFace + f) % 6);
-    }
+  if (buttonLongPressed()) {
+    signalTeam = 0;
+    scoringTeam = 0;
+    blinkType = FLICKER;
   }
 }
 
 void flickerDisplay() {
-  if (animTimer.isExpired()) {
-    spinFace = (spinFace + 1) % 6;
-    if (spinFace == 0) {
-      displayColor = ((displayColor + 1) % 3) + 1;
-    }
-    animTimer.set(ANIMATION_INTERVAL);
-  }
 
-  if (gameState == GAME) {//game/waiting display
-    if (scoringTeam > 0) {//we are connected to the game
-      setColor(OFF);
-
-      setColorOnFace(makeColorHSB(teamHues[scoringTeam], 255, 255), spinFace);
-
-      setColorOnFace(makeColorHSB(teamHues[signalTeam], 255, 255), (spinFace + 2) % 6);
-      setColorOnFace(makeColorHSB(teamHues[signalTeam], 255, 255), (spinFace + 3) % 6);
-      setColorOnFace(makeColorHSB(teamHues[signalTeam], 255, 255), (spinFace + 4) % 6);
-
-
-    } else {//we are just chillin
-      setColor(OFF);
-      setColorOnFace(makeColorHSB(teamHues[displayColor], 255, 255), spinFace);
-    }
-  } else {//showing final scoring team
-    setColor(makeColorHSB(teamHues[scoringTeam], 255, 255));
-  }
 }
 
-/////////////////////////
-//CONVENIENCE FUNCTIONS//
-/////////////////////////
+void flopperDisplay() {
 
-byte getBlinkType(byte data) {
-  return (data >> 5);
-}
-
-byte getGameState(byte data) {
-  return ((data >> 3) & 3);
-}
-
-byte getSignalTeam(byte data) {
-  return (data & 3);
-}
-
-bool transitionCheck() {//are any neighbors trying to send me into TRANSITION?
-  bool transitionBool = false;
-
-  FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) { //a neighbor
-      if (getGameState(getLastValueReceivedOnFace(f)) == TRANSITION) {//this neighbor wants me to transition
-        transitionBool = true;
-      }
-    }
-  }
-
-  return transitionBool;
-}
-
-bool scoreCheck() {//can I transition into SCORE mode safely?
-  bool scoreBool = true;
-
-  FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) { //a neighbor
-      if (getBlinkType(getLastValueReceivedOnFace(f)) == FLICKER) { //a neighbor I care about
-        if (getGameState(getLastValueReceivedOnFace(f)) == GAME) {//this neighbor still hasn't started their transition
-          scoreBool = false;
-        }
-      }
-    }
-  }
-
-  return scoreBool;
-}
-
-bool gameCheck() {//are any neighbors trying to send me into GAME?
-  bool gameBool = false;
-
-  FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) { //a neighbor
-      if (getBlinkType(getLastValueReceivedOnFace(f)) == FLICKER) { //a neighbor I care about
-        if (getGameState(getLastValueReceivedOnFace(f)) == GAME) {//this neighbor wants me to go back to game
-          gameBool = true;
-        }
-      }
-    }
-  }
-
-  return gameBool;
 }
